15b3e4c3c581c98e4450d8a18db7af07cfc20ee7
krishnas09 <113026236+krishnas09@users.noreply.github.com>
2022-09-07 16:31:59 +0530

    Add files via upload

---
+++ practice/Arthmetic.java
@@ -0,0 +1,15 @@
+//wrp to perform arthmetic operations on 3 nums;
+class Arthmetic
+{
+	public static void main(String[] args)
+	{
+		int a=10,b=20,c=15;
+		System.out.println("Before Arthmetic Operations Value of a = "+a+" b = "+b+" & c = "+c);
+		System.out.println("Performing '+' Operation : "+(a+b+c));
+		System.out.println("Performing '-' Operation : "+(a-b-c));
+		System.out.println("Performing '*' Operation : "+(a*b*c));
+		System.out.println("Performing '/' Operation on a/b : "+(a/b));
+		System.out.println("Performing '%' Operation on b%c : "+(b%c));
+		
+	}
+}
